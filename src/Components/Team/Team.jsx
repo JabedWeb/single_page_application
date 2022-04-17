a504d09fba237f3b3c0e66b51fbbf16052c4a601
jabed366 <jabedhasan231@gmail.com>
2022-04-17 17:23:54 +0600

    First Commit

---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Col, Container, Row,Button } from 'react-bootstrap';
+import './Team.css';
+
+const Team = () => {
+  return (
+    <section className='team'>
+       <Container className='my-5'>
+           <Row>
+               <Col md={3}>
+                <Card>
+                    <Card.Img style={{height :"200px", objectFit : "cover"}} src="https://cdn.powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png"/>
+                    <Card.Body>
+                        <h1>Jabed</h1>
+                        <p>01793534981</p>
+                        <Button>View Profile</Button>
+                    </Card.Body>
+                </Card>
+               </Col>
+           </Row>
+       </Container>
+    </section>
+  )
+}
+
+export default Team
